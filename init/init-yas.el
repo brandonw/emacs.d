523d9cfec68785962c5575d00cfd89f7d511e606
Brandon Waskiewicz <brandon.waskiewicz@gmail.com>
2013-11-10 12:03:39 -0500

    Remove unnecessary YAS settings

--- init/init-yas.el
+++ init/init-yas.el
@@ -5,18 +5,12 @@
 
 (require-package 'yasnippet)
 
-(defvar yas-fallback-behaviour 'return-nil)
-(defvar yas-also-auto-indent-first-line t)
-
 (require 'yasnippet)
 
 (add-to-list 'yas-snippet-dirs "~/.haskell-mode-snippets")
 (setq-default yas-prompt-functions '(yas-ido-prompt yas-dropdown-prompt))
 
 (yas-global-mode t)
-
-(define-key yas-minor-mode-map (kbd "TAB") nil)
-(define-key yas-minor-mode-map [tab] nil)
 
 (diminish 'yas-minor-mode)
 

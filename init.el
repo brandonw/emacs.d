e769fa3b9f23e11158f48621d79a5697880f3f79
Brandon Waskiewicz <brandon.waskiewicz@gmail.com>
2013-11-09 20:37:39 -0500

    Use new directory for major modes

--- init.el
+++ init.el
@@ -4,6 +4,7 @@
 ;;; Code:
 
 (add-to-list 'load-path (concat user-emacs-directory "init"))
+(add-to-list 'load-path (concat user-emacs-directory "lang"))
 (add-to-list 'load-path (concat user-emacs-directory "elisp"))
 
 (require 'init-setup)
@@ -12,13 +13,14 @@
 (require 'init-linum)
 (require 'init-ui)
 (require 'init-ido)
-(require 'init-haskell)
 (require 'init-yas)
 (require 'init-ac)
 (require 'init-git)
 (require 'init-flyspell)
 (require 'init-flycheck)
 (require 'init-projectile)
+
+(require 'lang-haskell)
 
 (provide 'init.el)
 ;;; init.el ends here
---
+++ lang/lang-haskell.el
@@ -0,0 +1,13 @@
+;;; lang-haskell.el --- Summary: Initialize haskell configuration
+;;; Commentary:
+
+;;; Code:
+
+(require-package 'haskell-mode)
+
+(eval-after-load 'haskell-mode
+  '(progn
+     ))
+
+(provide 'lang-haskell)
+;;; lang-haskell.el ends here
